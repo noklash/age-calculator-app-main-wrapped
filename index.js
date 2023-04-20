f5d53d6bae782efa7b7178439a346ae1eeac898a
noklash <123834419+noklash@users.noreply.github.com>
2023-04-20 16:38:33 +0300

    Added an onclick event listener to the submit button

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const yearEl = document.getElementById("year-el")
 const yearOutput = document.getElementById("year-output")
 const monthOutput = document.getElementById("month-output")
 const dayOutput = document.getElementById("day-output")
+const dayError = document.getElementById("day-error")
+const monthError = document.getElementById("month-error")
+const yearError = document.getElementById("year-error")
+const error = document.getElementsByClassName("error")
+
+
+function load(){
+    if(dayEl.value <= 31){
+        dayError.innerHTML = ""
+     }else {
+       dayError.innerHTML = "Must be a valid day"
+     }
+}
+ 
