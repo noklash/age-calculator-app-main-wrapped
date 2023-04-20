7681470c80d893f6a5c12b4b3f287c68496fe2ce
noklash <123834419+noklash@users.noreply.github.com>
2023-04-20 15:58:41 +0300

    declared variables in Javascript from the DOM

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const dayEl = document.getElementById("day-el")
+const monthEl = document.getElementById("month-el")
+const yearEl = document.getElementById("year-el")
+const yearOutput = document.getElementById("year-output")
+const monthOutput = document.getElementById("month-output")
+const dayOutput = document.getElementById("day-output")
