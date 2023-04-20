d37cf2316204d5107d1bd58b9aa032c37b789787
noklash <123834419+noklash@users.noreply.github.com>
2023-04-20 17:30:31 +0300

    Created a function for the errors

--- index.js
+++ index.js
@@ -9,13 +9,42 @@ const dayError = document.getElementById("day-error")
 const monthError = document.getElementById("month-error")
 const yearError = document.getElementById("year-error")
 const error = document.getElementsByClassName("error")
+const dayLab = document.getElementById("day-lab")
+const monthLab = document.getElementById("month-lab")
+const yearLab = document.getElementById("year-lab")
+let currentTime = new Date()
+let currentYear = currentTime.getFullYear()
 
 
-function load(){
+
+function day(){
     if(dayEl.value <= 31){
         dayError.innerHTML = ""
-     }else {
+     }else  {
        dayError.innerHTML = "Must be a valid day"
+       dayEl.style.borderColor = "red";
+       dayLab.style.color = "red";
      }
+}
+
+function month(){
+    if (monthEl.value <= 12){
+        monthError.innerHTML = ""
+    }else{
+        monthError.innerHTML = "Must be a valid month"
+        monthEl.style.borderColor = "red";
+        monthLab.style.color = "red";
+    }
+}
+
+function year(){
+    if (yearEl.value <= currentYear){
+        yearError.innerHTML = ""
+    }else {
+        yearError.innerHTML = "Must be in the past"
+        yearEl.style.borderColor = "red";
+        yearLab.style.color = "red";
+
+    }
 }
  
